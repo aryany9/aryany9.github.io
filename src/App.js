e1575bfdc48b05dda5874c6afbaf598c30c1d762
aryany9 <aryan9.00y@gmail.com>
2023-03-22 21:30:08 +0530

    Added env

--- src/App.js
+++ src/App.js
@@ -12,13 +12,6 @@ const Title = styled.h1`
 `
 
 function App() {
-
-  console.log(process.env.FIREBASE_APIKEY)
-  console.log(process.env.FIREBASE_AUTHDOMAIN)
-  console.log(process.env.FIREBASE_PROJECTID)
-  console.log(process.env.FIREBASE_STORAGEBUCKET)
-  console.log(process.env.FIREBASE_MESSAGINGSENDERID)
-  console.log(process.env.FIREBASE_APPID)
   return (
     <ThemeProvider theme={lightTheme}>
       <Homepage />
--- src/Firebase.js
+++ src/Firebase.js
@@ -8,24 +8,17 @@ import 'firebase/compat/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
+
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_APIKEY,
-    authDomain: process.env.FIREBASE_AUTHDOMAIN,
-    projectId: process.env.FIREBASE_PROJECTID,
-    storageBucket: process.env.FIREBASE_STORAGEBUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGINGSENDERID,
-    appId: process.env.FIREBASE_APPID,
-    measurementId: process.env.FIREBASE_MEASUREMENTID
+    apiKey: process.env.REACT_APP_APIKEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
+    appId: process.env.REACT_APP_FIREBASE_APPID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENTID
 };
 
-
-console.log(process.env.FIREBASE_APIKEY)
-console.log(process.env.FIREBASE_AUTHDOMAIN)
-console.log(process.env.FIREBASE_PROJECTID)
-console.log(process.env.FIREBASE_STORAGEBUCKET)
-console.log(process.env.FIREBASE_MESSAGINGSENDERID)
-console.log(process.env.FIREBASE_APPID)
-// Initialize Firebase
 export const app = firebase.initializeApp(firebaseConfig);
 export const analytics = firebaseAnalytics.getAnalytics(app);
 export const firestore = firebase.firestore();
